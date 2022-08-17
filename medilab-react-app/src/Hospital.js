9f5620f1c8d839d2c1b1e49a44ec044fea740850
satyajitnayakvc <110887391+satyajitnayakvc@users.noreply.github.com>
2022-08-17 17:10:55 +0530

    Update Hospital.js

--- medilab-react-app/src/Hospital.js
+++ medilab-react-app/src/Hospital.js
@@ -4,7 +4,7 @@ export class Hospital extends Component {
     render() {
         return (
             <div>
-                <h3>Welcoem to Evening Preclinic</h3>
+                <h3>Welcome to Evening Preclinic Satyajit</h3>
             </div>
         )
     }
