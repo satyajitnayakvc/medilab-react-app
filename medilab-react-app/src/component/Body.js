1d7e863c5087da51ace1577d1aa53742b4cd8a26
satyajitnayakvc <110887391+satyajitnayakvc@users.noreply.github.com>
2022-08-17 20:03:32 +0530

    Update Body.js

--- medilab-react-app/src/component/Body.js
+++ medilab-react-app/src/component/Body.js
@@ -6,7 +6,7 @@ export class Body extends Component {
             <div>
                 <section id="hero" class="d-flex align-items-center">
                     <div class="container">
-                    <h1>Welcome to Medilab</h1>
+                    <h1>Welcome to satyajit Medilab</h1>
                     <h2>We are team of talanted doctors making people healthier</h2>
                     <a href="#about" class="btn-get-started scrollto">Get Started</a>
                     </div>
